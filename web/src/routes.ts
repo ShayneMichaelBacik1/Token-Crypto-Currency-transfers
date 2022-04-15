e969208695cb966fbddeaa777e2b29dc63da3b3d
Shayne Michael Bacik <95336295+ShayneMichaelBacik1@users.noreply.github.com>
2022-04-15 14:13:09 -0600

    Initialize jake/no-json-rpc-url

---
+++ web/src/routes.ts
@@ -0,0 +1,12 @@
+// Copyright (c) 2018-2020 WalletLink.org <https://www.walletlink.org/>
+// Copyright (c) 2018-2020 Coinbase, Inc. <https://www.coinbase.com/>
+// Licensed under the Apache License, version 2.0
+
+export const routes = {
+  root: "/",
+  link: "/link",
+  linked: "/linked",
+  reset: "/reset",
+  bridge: "/bridge",
+  wallets: "/wallets"
+}
